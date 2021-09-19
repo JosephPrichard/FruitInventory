063a7fc4245ff1faadbe4d06cbf608a0949db0be
Joseph <Ltprichard2019@gmail.com>
2021-09-19 15:30:25 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ const port = 5000;
 app.use(express.json())
 
 //admin
-//B4mGjz8eQagVk9LT
-const CONNECTION_STRING = "mongodb+srv://admin:B4mGjz8eQagVk9LT@cluster0.t4eew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//<your_password>
+const CONNECTION_STRING = "mongodb+srv://admin:<your_password>@cluster0.t4eew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(CONNECTION_STRING).then(() => console.log("Connected to db"));
 
 app.use("/fruits", fruitRouter);
